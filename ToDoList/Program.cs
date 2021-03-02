3e7ea882e96e8669ed3c7b6aedd23a71bb5ebe8c
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-03-01 17:51:16 -0800

    refactor code for MVC config

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,43 +1,20 @@
-using System;
-using System.Collections.Generic;
-using ToDoList.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ToDoList
 {
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      Console.WriteLine("MAIN MENU");
-      Console.WriteLine("Enter 'A' to add a task, 'V' to view all tasks, or 'Q' to quit");
-      string userInput = Console.ReadLine().ToUpper();
-      switch (userInput)
-      {
-        case "A":
-          Console.WriteLine("Enter task to add");
-          string task = Console.ReadLine();
-          Item newItem = new Item(task);
-          Main();
-          break;
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-        case "V":
-          List<Item> allItems = Item.GetAll();
-          foreach (Item thisItem in allItems)
-          {
-            Console.WriteLine(thisItem.Description);
-          }
-          Main();
-          break;
-
-        case "Q":
-          Console.WriteLine("Good Bye!");
-          break;
-
-        default:
-          Console.WriteLine("Please enter a valid input");
-          Main();
-          break;
-      }
+      host.Run();
     }
   }
 }
