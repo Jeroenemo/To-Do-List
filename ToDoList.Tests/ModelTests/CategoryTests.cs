4afc81835105eab000a318fb1962bf64d8ee609c
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-03-03 20:13:57 -0800

    add getall method

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -6,21 +6,49 @@ using System;
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class CategoryTests
+  public class CategoryTests : IDisposable
   {
+    
+    public void Dispose()
+    {
+      Category.ClearAll();
+    }
 
     [TestMethod]
     public void CategoryConstructor_CreatesInstanceOfCategory_Category()
     {
       string name = "Test Category";
-      Category newCategory = new Category("test category");
+      Category newCategory = new Category("Test Category");
       
 
       string result = newCategory.Name;
 
       Assert.AreEqual(name, result);
+    }
 
-      
+    [TestMethod]
+    public void GetId_ReturnsCategoryId_Int()
+    {
+      string name = "Test Category";
+      Category newCategory = new Category(name);
+
+      int result = newCategory.Id;
+
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllCategoryObjects_CategoryList()
+    {
+      string name01 = "Work";
+      string name02 = "School";
+      Category newCategory1 = new Category(name01);
+      Category newCategory2 = new Category(name02);
+      List<Category> newList = new List<Category> { newCategory1, newCategory2 };
+
+      List<Category> result = Category.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
