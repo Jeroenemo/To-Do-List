294cf7ee94f7107075da8c43a25a8c4e061518f5
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-03-15 10:49:00 -0700

    deny item creation if category count == 0

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,14 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly ToDoListContext _db;
+
+      public HomeController(ToDoListContext db)
+      { 
+        _db = db;
+      }
+
 
       [HttpGet("/")]
       public ActionResult Index()
       {
-        return View();
+        List<Category> model = _db.Categories.ToList();
+        return View(model);
       }
 
       [Route("/favorite_photos")]
