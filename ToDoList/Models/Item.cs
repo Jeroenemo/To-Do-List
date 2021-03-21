9e03ddb26807f68a6ed4d240735b6ec836c7cc5b
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-03-21 13:05:56 -0700

    clean up white space

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -13,11 +13,11 @@ namespace ToDoList.Models
 
     public int ItemId { get; set; }
     public string Description { get; set; }
-    public bool IsComplete { get;  set; }
+    public bool IsComplete { get; set; }
     [DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     public DateTime DueDate { get; set; }
 
-    public virtual ICollection<CategoryItem> JoinEntities { get;}
+    public virtual ICollection<CategoryItem> JoinEntities { get; }
   }
 }
